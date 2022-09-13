f0655786051ab6756bad8cd19730089df8ffcdcd
Max Popsuy <maxim09p@gmail.com>
2022-09-13 18:22:56 +0300

    add session

---
+++ src/config/controllers/authController.js
@@ -0,0 +1,53 @@
+const bcrypt = require("bcrypt");
+const { user } = require("../../models");
+const auth = require("../../utils/auth");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    auth.isExistent(user, username, res);
+
+    const hashedPassword = await user.hashPassword(password);
+
+    const createdUser = await user.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    res.json({
+      user: createdUser,
+      token: await auth.generateToken(createdUser, "30m"),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const findedUser = await user.findOne({
+      username,
+    });
+    if (!findedUser) {
+      return res.status(422).send("Wrong credentials");
+    }
+
+    const isPasswordValid = await findedUser.validatePassword(password);
+    if (!isPasswordValid) {
+      return res.status(422).send("Wrong credentials");
+    }
+    res.json({
+      user: findedUser,
+      token: await auth.generateToken(findedUser, "30m"),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getProfile = async (req, res, next) => {
+  try {
+    res.json(req.user);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/config/controllers/usersController.js
+++ src/config/controllers/usersController.js
@@ -1,4 +1,4 @@
-const { user } = require("../models");
+const { user } = require("../../models");
 
 exports.update = async (req, res, next) => {
   try {
